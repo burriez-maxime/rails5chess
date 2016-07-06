3e674eae31b855f66c694125146d38a7ba1a33c1
Maxime Burriez <burriez_maxime@hotmail.com>
2016-07-06 11:34:53 +0200

    update redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = nil
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
